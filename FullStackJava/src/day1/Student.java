bf27cc69e55859146e87e88024f7a5706dd94f29
Akanksha Meshram <akankshameshram989@gmail.com>
2025-12-17 15:05:50 +0530

    Initial commit

---
+++ FullStackJava/src/day1/Student.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Student {
+     String name;
+     int age;
+     String gender;
+     
+     void study() {
+    	 System.out.println("Student is stduying");
+     }
+}
+
+
---
+++ FullStackJava/src/day1/StudentApp.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class StudentApp {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.name= "Akanksha Meshram";
+
+	}
+
+}

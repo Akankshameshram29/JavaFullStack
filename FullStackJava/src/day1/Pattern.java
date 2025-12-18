df71b807961cd435d2533cb43aa26268b345dac7
Akanksha Meshram <akankshameshram989@gmail.com>
2025-12-18 15:45:54 +0530

    day 3 added

--- FullStackJava/src/day1/Pattern.java
+++ FullStackJava/src/day1/Pattern.java
@@ -3,10 +3,60 @@ package day1;
 public class Pattern {
 
 	public static void main(String[] args) {
+		// pattern - *****
 		for(int i =0;i<5;i++) {
-			System.out.println("*");		
+			System.out.print("*");		
 			}
-
+       
+		/*pattern *
+		          *
+		          *
+		          *
+		          *       */
+		
+		for(int i =0;i<=5;i++) {
+			System.out.println("*");
+		}
+		
+		/* pattern *****
+		           *****
+		           *****
+		           *****
+		           *****
+		 */
+		
+		for(int i =0;i<5;i++) {
+			for(int j =0;j<5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		/*  *****  <-pattern
+		    *   *
+		    *   *
+		    *   *
+		    *****
+		    
+		 */
+		
+		for(int i =0;i<5;i++) {
+			if(i==0 || i==4) {
+				for(int j =0;j<5;j++) {
+					System.out.print("*");
+				}
+			}
+			else{
+				for(int j=0;j<5;j++) {
+					if(j==0 || j==4) {
+						System.out.print("*");
+					}else {
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.println("");
+		}
 	}
 
 }
--- FullStackJava/src/day1/StudentApp.java
+++ FullStackJava/src/day1/StudentApp.java
@@ -4,7 +4,18 @@ public class StudentApp {
 
 	public static void main(String[] args) {
 		Student s1 = new Student();
-		s1.name= "Akanksha Meshram";
+		System.out.println("Before Assigning");
+		System.out.println(s1.age);
+		System.out.println(s1.name);
+		System.out.println(s1.gender);
+		System.out.println("After assigning value");
+	    s1.name= "Akanksha Meshram";
+	    s1.age = 22;
+	    s1.gender = "female";
+	    System.out.println(s1.name);
+	    System.out.println(s1.age);
+	    System.out.println(s1.gender);
+		
 
 	}
 
